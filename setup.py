8949f50b31b5d8d67cbaeb0e0c4521b62aed22ab
unparalleled-js <yingthi@live.com>
2021-10-13 15:30:59 -0500

    fix: upgrades

--- setup.py
+++ setup.py
@@ -60,10 +60,10 @@ setup(
     url="https://github.com/ApeWorX/ape-infura",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.1.0a21",
+        "eth-ape>=0.1.0a24",
         "importlib-metadata ; python_version<'3.8'",
     ],  # NOTE: Add 3rd party libraries here
-    python_requires=">=3.6,<4",
+    python_requires=">=3.7,<4",
     extras_require=extras_require,
     py_modules=["ape_infura"],
     license="Apache-2.0",
