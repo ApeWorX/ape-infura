9d8eea750666ab33652be0f00255e7ef3b19bc80
unparalleled-js <yingthi@live.com>
2021-11-09 17:40:04 -0600

    chore: upgrade ape

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     url="https://github.com/ApeWorX/ape-infura",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.1.0a21",
+        "eth-ape>=0.1.0a26",
         "importlib-metadata ; python_version<'3.8'",
     ],  # NOTE: Add 3rd party libraries here
     python_requires=">=3.7,<4",
