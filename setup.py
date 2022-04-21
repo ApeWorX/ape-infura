09778fc06ba5af200cf20039a69ae7e5c4487ae3
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2022-04-21 12:18:33 +0200

    chore: bump ape core version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ extras_require = {
         "hypothesis>=6.2.0,<7.0",  # Strategy-based fuzzer
     ],
     "lint": [
-        "black>=21.10b0,<22.0",  # auto-formatter and linter
+        "black>=22.3.0,<23.0",  # auto-formatter and linter
         "mypy>=0.910,<1.0",  # Static type analyzer
         "flake8>=3.9.2,<4.0",  # Style linter
         "isort>=5.9.3,<6.0",  # Import sorting linter
@@ -60,7 +60,7 @@ setup(
     url="https://github.com/ApeWorX/ape-infura",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.1.0,<0.2.0",
+        "eth-ape>=0.2.1,<0.3.0",
         "importlib-metadata ; python_version<'3.8'",
     ],  # NOTE: Add 3rd party libraries here
     python_requires=">=3.7.2,<4",
