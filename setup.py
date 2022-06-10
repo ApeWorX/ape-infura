4f83f34404805f70e46d57de88a1557bba6d1c5b
Juliya Smith <yingthi@live.com>
2022-06-10 08:39:22 -0500

    refactor: use shared web3 provider logic (#37)

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     url="https://github.com/ApeWorX/ape-infura",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.2.2,<0.3.0",
+        "eth-ape>=0.2.8,<0.3.0",
         "importlib-metadata ; python_version<'3.8'",
     ],  # NOTE: Add 3rd party libraries here
     python_requires=">=3.7.2,<4",
