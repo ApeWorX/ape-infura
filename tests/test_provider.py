39a326aa18e17e1cf288f79454060d3e11a3f6dd
unparalleled-js <yingthi@live.com>
2022-09-12 15:00:54 -0500

    test: adjust tests

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -10,16 +10,29 @@ from ape_infura.providers import Infura
         ("ethereum", "mainnet"),
         ("ethereum", "kovan"),
         ("arbitrum", "mainnet"),
-        # Uncomment when network name corrected
-        # ("arbitrum", "testnet"),
         ("polygon", "mumbai"),
     ],
 )
-def test_ethereum_mainnet(ecosystem, network):
+def test_provider(ecosystem, network):
     ecosystem_cls = networks.get_ecosystem(ecosystem)
     network_cls = ecosystem_cls.get_network(network)
     with network_cls.use_provider("infura") as provider:
         assert isinstance(provider, Infura)
         assert provider.get_balance("0x0000000000000000000000000000000000000000") > 0
-        ecosystem_uri = "" if ecosystem == "ethereum" else f"{ecosystem}-"
-        assert f"https://{ecosystem_uri}{network}.infura.io/v3/" in provider.uri
+        assert f"https://{ecosystem}-{network}.infura.io/v3/" in provider.uri
+
+
+@pytest.mark.parametrize("network", "mainnet", "kovan")
+def test_ethereum(networks, network):
+    network = networks.ethereum.get_network(networks)
+    with network.use_provider("infura") as provider:
+        assert isinstance(provider, Infura)
+        assert provider.get_balance("0x0000000000000000000000000000000000000000") > 0
+        assert f"https://{network}.infura.io/v3/" in provider.uri
+
+
+def test_arbitrum_testnet():
+    with networks.arbitrum.testnet.use_provider("infura") as provider:
+        assert isinstance(provider, Infura)
+        assert provider.get_balance("0x0000000000000000000000000000000000000000") > 0
+        assert f"https://arbitrum-rinkeby.infura.io/v3/" in provider.uri
