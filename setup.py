a297ef54a08b933142eaa0abdecf64808f246dc1
unparalleled-js <yingthi@live.com>
2023-01-31 13:40:19 -0600

    chore: bump ape

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     url="https://github.com/ApeWorX/ape-infura",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.5.2,<0.6",
+        "eth-ape>=0.6.0,<0.7",
     ],
     python_requires=">=3.8,<3.11",
     extras_require=extras_require,
