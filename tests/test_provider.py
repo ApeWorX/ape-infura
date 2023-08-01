47faf1f7b2caeff282a3f50c1b9289d867f9fa02
dawnseeker8 <dawnseeker8@gmail.com>
2023-08-01 21:55:35 +0800

    feat: add linea network[APE-1170] * add Linea network * commit the missing files from last push to fix CI pipeline

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -17,6 +17,8 @@ from ape_infura.provider import Infura
         ("optimism", "goerli"),
         ("polygon", "mainnet"),
         ("polygon", "mumbai"),
+        ["linea", "mainnet"],
+        ["linea", "goerli"],
     ],
 )
 def test_infura(ecosystem, network):
