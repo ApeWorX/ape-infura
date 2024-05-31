857b699f5d720a1a2503a3cef103fcd447207b00
Dalena <mimidalena@gmail.com>
2024-05-31 15:07:31 -0500

    chore: bump ape

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     url="https://github.com/ApeWorX/ape-infura",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.7.0,<0.8",
+        "eth-ape>=0.8.1,<0.9",
     ],
     python_requires=">=3.9,<4",
     extras_require=extras_require,
