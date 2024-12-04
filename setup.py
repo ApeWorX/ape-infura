90491d02dd5890d5be5b9eca8557f8d23af23d70
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2024-12-04 16:35:39 -0500

    chore: update pins

--- setup.py
+++ setup.py
@@ -76,8 +76,8 @@ setup(
     include_package_data=True,
     install_requires=[
         "eth-ape>=0.8.1,<0.9",
-        "web3>=7.6.0,<8",
-        "requests>=2.32.3,<3",
+        "web3>=6.20.3,<8",
+        "requests>=2.28.1,<3",
     ],
     python_requires=">=3.9,<4",
     extras_require=extras_require,
